61db22581bf03a34e896e0d475485229a3abb4f5
Lukas <info@brainlesslukas.com>
2026-03-12 22:25:10 +0100

    feat: hyprpanel config

--- modules/services/hyprpanel.nix
+++ modules/services/hyprpanel.nix
@@ -1,44 +1,30 @@
-{ inputs, ... }:
-{
-  programs.hyprpanel = {
-    # Configure and theme almost all options from the GUI.
-    # See 'https://hyprpanel.com/configuration/settings.html'.
-    # Default: <same as gui>
-    settings = {
+{ pkgs, ... }:
 
-      # Configure bar layouts for monitors.
-      # See 'https://hyprpanel.com/configuration/panel.html'.
-      # Default: null
-      layout = {
+{
+  home-manager.users.lukas = {
+    programs.hyprpanel = {
+      enable = true;
+      settings = {
         bar.layouts = {
-          "0" = {
-            left = [ "dashboard" "workspaces" ];
+          "*" = {
+            left = [
+              "dashboard"
+              "workspaces"
+            ];
             middle = [ "media" ];
-            right = [ "volume" "systray" "notifications" ];
+            right = [
+              "clock"
+              "notifications"
+            ];
           };
         };
-      };
-
-      bar.launcher.autoDetectIcon = true;
-      bar.workspaces.show_icons = true;
-
-      menus.clock = {
-        time = {
-          military = true;
-          hideSeconds = true;
-        };
-        weather.unit = "metric";
-      };
-
-      menus.dashboard.directories.enabled = false;
-      menus.dashboard.stats.enable_gpu = true;
-
-      theme.bar.transparent = true;
-
-      theme.font = {
-        name = "CaskaydiaCove NF";
-        size = "16px";
+        bar.clock.format = "%H:%M";
+        bar.clock.icon = false;
+        bar.launcher.autoDetectIcon = true;
+        bar.workspaces.workspaces = 2;
+        menus.clock.time.military = true;
       };
     };
   };
 }
+
