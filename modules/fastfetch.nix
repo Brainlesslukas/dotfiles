df57ae2babf4bbebb1d41878c2695858c2f71d56
Lukas <info@brainlesslukas.com>
2026-03-13 15:22:57 +0100

    fix: render nix logo

--- modules/fastfetch.nix
+++ modules/fastfetch.nix
@@ -6,38 +6,52 @@
       enable = true;
       settings = {
         logo = {
-          source = "nixos_small";
+          type = "kitty-direct";
+          source = "~/Wallpaper/nix-snowflake.png";
+          width = 18;
+          height = 8;
+          padding.top = 1;
         };
+        display.separator = "  ";
         modules = [
+          "break"
+          "title"
           {
             type = "os";
             format = "I use {pretty-name} btw.";
+            keyColor = "red";
           }
           {
             type = "wm";
             format = "{pretty-name} {version}";
+            keyColor = "green";
           }
           {
             type = "kernel";
             format = "{sysname} {release}";
+            keyColor = "yellow";
           }
           {
             type = "disk";
             key = "Disk";
             format = "{size-used} / 500GiB";
+            keyColor = "blue";
           }
           {
             type = "memory";
             key = "RAM";
             format = "{used} / 16GiB";
+            keyColor = "magenta";
           }
           {
             type = "packages";
             format = "{all}";
+            keyColor = "cyan";
           }
           {
             type = "uptime";
             format = "{formatted}";
+            keyColor = "red";
           }
         ];
       };
