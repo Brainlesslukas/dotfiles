02803ee85c862ee42ebf6378eb92e3e60be5793e
Lukas <info@brainlesslukas.com>
2026-08-17 23:14:11 +0200

    chore: sudo-rs

--- modules/security/default.nix
+++ modules/security/default.nix
@@ -5,5 +5,13 @@
       self.nixosModules.modulesSecurityVpn
       self.nixosModules.modulesSecurityEncryption
     ];
+    security = {
+      sudo.enable = false;
+      sudo-rs = {
+        enable = true;
+        execWheelOnly = true;
+        wheelNeedsPassword = true;
+      };
+    };
   };
 }
